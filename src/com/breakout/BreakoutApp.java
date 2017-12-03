98b48be08065c1241540e0043577963c54e979d9
Batsirai Swiswa <batsiraiswiswa@yahoo.com>
2017-12-03 18:08:05 -0500

    Import FXGL library into classpath. Extend GameApplication class

---
+++ src/com/breakout/BreakoutApp.java
@@ -0,0 +1,51 @@
+package com.breakout;
+
+import com.almasb.fxgl.GameApplication;
+import com.almasb.fxgl.GameSettings;
+
+import javafx.scene.layout.Pane;
+
+public class BreakoutApp extends GameApplication{
+
+	@Override
+	protected void initSettings(GameSettings settings) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void initAssets() throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void initGame() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void initUI(Pane uiRoot) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void initInput() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void onUpdate() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+
+	}
+
+}
