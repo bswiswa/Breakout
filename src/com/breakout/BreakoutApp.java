71418ca549586d0faf7e439b86a0207f937f29ac
Batsirai Swiswa <batsiraiswiswa@yahoo.com>
2017-12-04 07:02:20 -0500

    Add bricks to field

--- src/com/breakout/BreakoutApp.java
+++ src/com/breakout/BreakoutApp.java
@@ -38,7 +38,7 @@ public class BreakoutApp extends GameApplication{
 
 	@Override
 	protected void initGame() {
-		//remove gravity
+		//remove gravity default is (0, 10)
 		physicsManager.setGravity(0,0);
 		initBat();
 		initBall();
@@ -46,14 +46,9 @@ public class BreakoutApp extends GameApplication{
 		
 	}
 
-	private void initBricks() {
-		// TODO Auto-generated method stub
-		
-	}
-
 	private void initBat() {
 		bat = new PhysicsEntity(Type.BAT);
-		bat.setPosition((getWidth()/2 - 128/2), (getHeight() - 40));
+		bat.setPosition((getWidth()/2 - 128/2), (getHeight() - 25));
 		bat.setGraphics(assets.getTexture("bat.png"));
 		bat.setBodyType(BodyType.KINEMATIC);
 		addEntities(bat);
@@ -61,10 +56,25 @@ public class BreakoutApp extends GameApplication{
 	
 	private void initBall() {
 		ball = new PhysicsEntity(Type.BALL);
-		ball.setPosition((getWidth()/2) - 30/2, (getHeight()/2 - 30/2));
+		ball.setPosition(getWidth()/2 - 30/2, getHeight()/2 -30/2);
 		ball.setGraphics(assets.getTexture("ball.png"));
 		ball.setBodyType(BodyType.DYNAMIC);
 		addEntities(ball);
+	}
+	
+	private void initBricks() {
+		for(int i = 0; i < 48; i++) {
+			PhysicsEntity brick = new PhysicsEntity(Type.BRICK);
+			brick.setPosition((i%16)*40, (i/16)*40);
+			/* there are 16 bricks that fit in a row so each row
+			 * we specify the position by i%16 since it will keep repeating between 1 -16
+			 * whereas for height, we want the height to change after 16 so instead of i%16 we do i/16
+			 */
+			brick.setGraphics(assets.getTexture("brick.png"));
+			
+			addEntities(brick);
+		}
+		
 	}
 
 	@Override
