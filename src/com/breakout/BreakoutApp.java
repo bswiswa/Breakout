74e2d25efb2a7815f8e484b457c2cc94c696eb95
Batsirai Swiswa <batsiraiswiswa@yahoo.com>
2017-12-04 22:53:48 -0500

    Complete game, add loss of 1000 points if player doesn't catch the ball

--- src/com/breakout/BreakoutApp.java
+++ src/com/breakout/BreakoutApp.java
@@ -7,10 +7,14 @@ import org.jbox2d.dynamics.FixtureDef;
 import com.almasb.fxgl.GameApplication;
 import com.almasb.fxgl.GameSettings;
 import com.almasb.fxgl.asset.Assets;
+import com.almasb.fxgl.entity.Entity;
 import com.almasb.fxgl.entity.EntityType;
+import com.almasb.fxgl.physics.CollisionHandler;
 import com.almasb.fxgl.physics.PhysicsEntity;
 import com.almasb.fxgl.physics.PhysicsManager;
 
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.geometry.Point2D;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.Pane;
@@ -24,15 +28,16 @@ public class BreakoutApp extends GameApplication{
 	}
 	
 	private PhysicsEntity bat, ball;
-	
 	private Assets assets;
+	private IntegerProperty score = new SimpleIntegerProperty();
+	
 	@Override
 	protected void initSettings(GameSettings settings) {
 		settings.setTitle("Breakout");
 		settings.setVersion("dev");
 		settings.setWidth(640);
 		settings.setHeight(740);
-		settings.setIntroEnabled(false);
+		settings.setIntroEnabled(true);
 		
 		
 	}
@@ -48,32 +53,77 @@ public class BreakoutApp extends GameApplication{
 	protected void initGame() {
 		//remove gravity default is (0, 10)
 		physicsManager.setGravity(0,0);
-		initScreenBounds();
 		initBat();
 		initBall();
 		initBricks();
+		initScreenBounds();
+		
+		physicsManager.addCollisionHandler(new CollisionHandler(Type.BALL, Type.BRICK) {
+
+			@Override
+			public void onCollisionBegin(Entity a, Entity b) {
+				removeEntity(b);
+				score.set(score.get()+100);
+				
+			}
+
+			@Override
+			public void onCollision(Entity a, Entity b) {	
+			}
+
+			@Override
+			public void onCollisionEnd(Entity a, Entity b) {	
+			}
+			
+		});
+		
+		//to catch collision with the bottom of the screen
+		//player loses 1000 points if they fail to catch the ball.
+		physicsManager.addCollisionHandler(new CollisionHandler(Type.BALL, Type.SCREEN) {
+
+			@Override
+			public void onCollisionBegin(Entity a, Entity b) {
+				removeEntity(b);
+				score.set(score.get()-1000);
+				
+			}
+
+			@Override
+			public void onCollision(Entity a, Entity b) {	
+			}
+
+			@Override
+			public void onCollisionEnd(Entity a, Entity b) {	
+			}
+			
+		});
 		
 	}
 	
 	private void initScreenBounds() {
 		PhysicsEntity top = new PhysicsEntity(Type.SCREEN);
-		top.setPosition(0, -10);
-		top.setGraphics(new Rectangle(getWidth(), 10));
+		top.setPosition(0, -5);
+		top.setGraphics(new Rectangle(getWidth(), 5));
 		
 		PhysicsEntity bottom = new PhysicsEntity(Type.SCREEN);
 		bottom.setPosition(0, getHeight());
-		bottom.setGraphics(new Rectangle(getWidth(), 10));
+		bottom.setGraphics(new Rectangle(getWidth(), 5));
+		bottom.setCollidable(true);
 		
 		PhysicsEntity left = new PhysicsEntity(Type.SCREEN);
-		left.setPosition(-10, 0);
-		left.setGraphics(new Rectangle(10, getHeight()));
+		left.setPosition(-5, 0);
+		left.setGraphics(new Rectangle(5, getHeight()));
 		
 		PhysicsEntity right = new PhysicsEntity(Type.SCREEN);
 		right.setPosition(getWidth(), 0);
-		right.setGraphics(new Rectangle(10, getHeight()));
+		right.setGraphics(new Rectangle(5, getHeight()));
 		
 		
-		addEntities(top, bottom, left, right);
+		addEntities(top);
+		addEntities(left);
+		addEntities(bottom);
+		addEntities(right);
+		
 	}
 
 	private void initBat() {
@@ -90,6 +140,8 @@ public class BreakoutApp extends GameApplication{
 		ball.setPosition(getWidth()/2 - 30/2, getHeight()/2 -30/2);
 		ball.setGraphics(assets.getTexture("ball.png"));
 		ball.setBodyType(BodyType.DYNAMIC);
+		//so that the collision event fires (setCollidable is false by default because Colliding is an expensive operation)
+		ball.setCollidable(true);
 		
 		FixtureDef fd = new FixtureDef();
 		fd.restitution = 0.8f;
@@ -110,7 +162,7 @@ public class BreakoutApp extends GameApplication{
 			 * whereas for height, we want the height to change after 16 so instead of i%16 we do i/16
 			 */
 			brick.setGraphics(assets.getTexture("brick.png"));
-			
+			brick.setCollidable(true);
 			addEntities(brick);
 		}
 		
@@ -121,7 +173,7 @@ public class BreakoutApp extends GameApplication{
 		Text scoreText = new Text();
 		scoreText.setTranslateY(50);
 		scoreText.setFont(Font.font(18));
-		scoreText.setText("SCORE:");
+		scoreText.textProperty().bind(score.asString());
 		uiRoot.getChildren().add(scoreText);
 		
 	}
